57567e2bad0505029f31d933da99442522776d58
Rakindu Niwunhella <rakindu15@gmail.com>
2025-11-17 22:58:49 +0530

    Added Landing Page
    Added a landing page to the front-end folder, and structured the folder with the necessary components

---
+++ Front-End/LandingPage/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Overview from './Components/overview'
+import Mission from './Components/Mission'
+import Goals from './Components/Goals'
+import Features from './Components/Features'
+import Footer from './Components/Footer'
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Overview/>
+    <Mission/>
+    <Goals/>
+    <Features/>
+    <Footer/>
+
+</>
+  )
+}
+
+export default App
