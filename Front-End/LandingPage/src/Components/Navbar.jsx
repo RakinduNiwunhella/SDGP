e46458fff84c3b8e4ed9da58075d91e2b6079061
Rakindu Niwunhella <rakindu15@gmail.com>
2025-11-22 21:37:27 +0530

    added animations to the register page
    added subtle animations when opening the registration page, and made the button round

--- Front-End/LandingPage/src/Components/Navbar.jsx
+++ Front-End/LandingPage/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({ onOpenRegister }) => {
     <nav className="w-full flex items-center justify-between px-10 py-4 shadow-sm bg-white">
 
       <div className="flex items-center gap-2">
-        <span className="text-2xl font-bold">MyLogo</span>
+        <span className="text-2xl font-bold ">MyLogo</span>
       </div>
 
       <div className="flex items-center gap-10 text-sm font-semibold">
@@ -19,7 +19,7 @@ const Navbar = ({ onOpenRegister }) => {
       <div className="flex items-center gap-4">
         <button
           onClick={onOpenRegister}
-          className="bg-green-700 text-white px-6 py-2 font-semibold hover:bg-green-800"
+          className="bg-green-700 text-white px-6 py-2 font-semibold hover:bg-green-800 rounded-md "
         >
           Register
         </button>
