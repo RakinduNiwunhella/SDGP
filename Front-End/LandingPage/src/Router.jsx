5193ff1c66a2df7929c04e1f46e60ae0d5adb70b
Sithuli Basnayake <sithulibasnayake22@gmail.com>
2025-12-09 23:07:21 +0530

    up

--- Front-End/LandingPage/src/Router.jsx
+++ Front-End/LandingPage/src/Router.jsx
@@ -1,10 +1,31 @@
-import { Link } from "react-router-dom"; 
+import { createBrowserRouter } from "react-router-dom";
+import Overview from "./Pages/Overview";
+import Mission from "./Pages/Mission";
+import Goal from "./Pages/Goal";
+import Features from "./Pages/Features";
+import Contact from "./Pages/Contact";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Overview />,
+  },
+  {
+    path: "/Mission",
+    element: <Mission />,
+  },
+  {
+    path: "/Goal",
+    element: <Goal />,
+  },
+  {
+    path: "/Features",
+    element: <Features />,
+  },
+  {
+    path: "/contact-us",
+    element: <Contact />,
+  },
+]);
 
-<ul className="space-y-2 text-gray-300 text-sm">
-  <li><Link to="/" className="hover:text-white transition">Overview</Link></li>
-  <li><Link to="/about" className="hover:text-white transition">Mission</Link></li>
-  <li><Link to="/services" className="hover:text-white transition">Goal</Link></li>
-  <li><Link to="/contact" className="hover:text-white transition">Features</Link></li>
-  <li><Link to="/contact" className="hover:text-white transition">Contact Us</Link></li>
-</ul>
+export default router;
