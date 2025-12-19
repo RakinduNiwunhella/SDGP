0d7ed614b7c6244ac47e8b519a4ffa73d265001f
Chethina2003 <kovida.fernando@gmail.com>
2025-12-19 20:22:13 +0530

    dark mod added to contact.jsx

--- Front-End/LandingPage/src/Components/Contact.jsx
+++ Front-End/LandingPage/src/Components/Contact.jsx
@@ -4,44 +4,44 @@ import { Mail, Phone, Ticket } from "lucide-react";
 function Contact() {
   return (
     <section id="contact">
-    <div className="w-full bg-gray-50 py-20 px-6 md:px-12">
+    <div className="w-full bg-gray-50 py-20 px-6 md:px-12 dark:bg-gray-900">
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-20 items-start">
 
         {/* LEFT SIDE FORM */}
-        <div className="bg-white p-10 rounded-2xl shadow-lg border border-gray-100">
-          <h2 className="text-3xl font-bold text-gray-800 mb-8">Contact <span className="font-bold text-green-600">Us</span></h2>
+        <div className="bg-white p-10 rounded-2xl shadow-lg border border-gray-100 dark:bg-black dark:border-gray-800">
+          <h2 className="text-3xl font-bold text-gray-800 mb-8 dark:text-white">Contact <span className="font-bold text-green-600 dark:text-green-400">Us</span></h2>
 
           <form className="space-y-6">
             <div className="grid grid-cols-2 gap-4">
               <input
                 type="text"
                 placeholder="First Name"
-                className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200"
+                className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200 dark:bg-black dark:border-gray-700 dark:placeholder-gray-500"
               />
               <input
                 type="text"
                 placeholder="Last Name"
-                className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200"
+                className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200 dark:bg-black dark:border-gray-700 dark:placeholder-gray-500"
               />
             </div>
 
             <input
               type="text"
               placeholder="Phone Number"
-              className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200"
+              className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200 dark:bg-black dark:border-gray-700 dark:placeholder-gray-500"
             />
 
             <textarea
               placeholder="Message"
               rows="6"
-              className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200"
+              className="border border-gray-200 rounded-lg px-4 py-3 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-200 dark:bg-black dark:border-gray-700 dark:placeholder-gray-500"
             ></textarea>
 
 
 
             <button
               type="button"
-              className="w-full bg-black text-white py-3 rounded-lg text-sm font-semibold hover:bg-gray-900 transition"
+              className="w-full bg-black text-white py-3 rounded-lg text-sm font-semibold hover:bg-gray-900 transition dark:bg-gray-600 dark:hover:bg-green-500"
             >
               SEND MESSAGE
             </button>
@@ -50,10 +50,10 @@ function Contact() {
 
         {/* RIGHT SIDE INFO */}
         <div className="flex flex-col justify-center mt-8 md:mt-20">
-          <h2 className="text-4xl font-bold text-gray-800 mb-4">
-            Get In <span className="text-green-600"> Touch</span>
+          <h2 className="text-4xl font-bold text-gray-800 mb-4 dark:text-white">
+            Get In <span className="text-green-600 dark:text-green-500"> Touch</span>
           </h2>
-          <p className="text-gray-600 leading-relaxed mb-10 max-w-md">
+          <p className="text-gray-600 leading-relaxed mb-10 max-w-md dark:text-gray-300">
 Need help? Get in touch with us.          </p>
 
           <div className="space-y-6">
@@ -62,18 +62,18 @@ Need help? Get in touch with us.          </p>
                 <Phone className="w-4 h-4" />
               </div>
               <div>
-                <div className="text-sm text-gray-500">Phone</div>
-                <div className="font-semibold text-gray-900">+94 77 249 2154</div>
+                <div className="text-sm text-gray-500 dark:text-gray-200">Phone</div>
+                <div className="font-semibold text-gray-900  dark:text-gray-200">+94 77 249 2154</div>
               </div>
             </div>
 
             <div className="flex items-center gap-4">
-              <div className="bg-black text-white rounded-full p-3">
+              <div className="bg-black text-white rounded-full p-3 ">
                 <Mail className="w-4 h-4" />
               </div>
               <div>
-                <div className="text-sm text-gray-500">Email</div>
-                <div className="font-semibold text-gray-900">ricevisionlanka@gmail.com</div>
+                <div className="text-sm text-gray-500  dark:text-gray-200">Email</div>
+                <div className="font-semibold text-gray-900  dark:text-gray-200">ricevisionlanka@gmail.com</div>
               </div>
             </div>
 
