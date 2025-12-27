c4e8f5f34dfc5111e753741fa3fe3a58df02fcc7
Binada <binada2005@gmail.com>
2025-12-27 20:53:33 +0530

    Backend structure created

--- Front-End/SDGP/src/Components/Sites/FieldData.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FieldData = () => {
-  return (
-    <div>FieldData</div>
-  )
-}
-
-export default FieldData
--- Front-End/SDGP/src/Components/Sites/FieldMap.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FieldMap = () => {
-  return (
-    <div>FieldMap</div>
-  )
-}
-
-export default FieldMap
